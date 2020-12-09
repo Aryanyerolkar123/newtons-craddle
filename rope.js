d770405156fc8e1c27d89add8950cab6e36fd67e
Aryanyerolkar123 <74817573+Aryanyerolkar123@users.noreply.github.com>
2020-12-09 16:45:46 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,15 @@
+constructor (body1, body2, offsetX , offsetY)
+{
+
+    this.offsetX = offsetX
+    this.offsetY = offsetY
+    var options = {
+        bodyA : body1,
+        bodyB : body2,
+        pointB : {x : this.offsetX, y : this.offsetY}
+    }
+    //console.log (options);
+    this.rope = constraint.create(options)
+    World.add (world, this.rope)
+}
+ 
